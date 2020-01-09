9c266da26305acdcee2d5c8ec6d0d75cd237ef64
andychen <andychen1555@163.com>
2020-01-09 19:59:49 +0800

    chapter 8-7 data文件数据导入到索引对象中

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/handler/AdLevelDataHandler.java
@@ -0,0 +1,80 @@
+package com.imooc.ad.handler;
+
+import com.imooc.ad.dump.table.AdCreativeTable;
+import com.imooc.ad.dump.table.AdPlanTable;
+import com.imooc.ad.index.DataTable;
+import com.imooc.ad.index.IndexAware;
+import com.imooc.ad.index.adplan.AdPlanIndex;
+import com.imooc.ad.index.adplan.AdPlanObject;
+import com.imooc.ad.index.creative.CreativeIndex;
+import com.imooc.ad.index.creative.CreativeObject;
+import com.imooc.ad.mysql.constant.OpType;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 1. 索引之间存在着层级的划分, 也就是依赖关系的划分
+ * 2. 加载全量索引其实是增量索引 "添加" 的一种特殊实现
+ */
+@Slf4j
+public class AdLevelDataHandler {
+
+  /**
+   * 操作 adPlanIndex 索引， ADD UPDATE DELETE
+   * @param planTable
+   * @param type
+   */
+  public static void handleLevel2(AdPlanTable planTable,
+                                  OpType type) {
+    AdPlanObject adPlanObject = new AdPlanObject(
+        planTable.getId(),
+        planTable.getUserId(),
+        planTable.getPlanStatus(),
+        planTable.getStartDate(),
+        planTable.getEndDate()
+    );
+    handleBinlogEvent(DataTable.of(AdPlanIndex.class),
+                     adPlanObject.getPlanId(),
+                     adPlanObject,
+                     type);
+  }
+
+  public static void handleLevel2(AdCreativeTable creativeTable,
+                                  OpType type) {
+
+    CreativeObject creativeObject = new CreativeObject(
+        creativeTable.getAdId(),
+        creativeTable.getName(),
+        creativeTable.getType(),
+        creativeTable.getMaterialType(),
+        creativeTable.getHeight(),
+        creativeTable.getWidth(),
+        creativeTable.getAuditStatus(),
+        creativeTable.getAdUrl()
+    );
+    handleBinlogEvent(
+        DataTable.of(CreativeIndex.class),
+        creativeObject.getAdId(),
+        creativeObject,
+        type
+    );
+  }
+
+  private static <K, V> void handleBinlogEvent(IndexAware<K, V> index,
+                                              K key,
+                                              V value,
+                                              OpType type) {
+    switch (type) {
+      case ADD:
+        index.add(key, value);
+        break;
+      case UPDATE:
+        index.update(key, value);
+        break;
+      case DELETE:
+        index.delete(key, value);
+        break;
+      default:
+        break;
+    }
+  }
+}
